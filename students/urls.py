45308af26d4aa2005ebd2ebbd33bb395f0700662
unknown <rithinshibujohn@gmail.com>
2026-07-06 13:24:26 +0530

    Working project before Student CRUD

--- students/urls.py
+++ students/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path("", views.student_list, name="student_list"),
+    path("", views.StudentListView.as_view(), name="student_list"),
+    path("add/", views.StudentCreateView.as_view(), name="student_add"),
+    path("<int:pk>/", views.StudentDetailView.as_view(), name="student_detail"),
+    path("<int:pk>/edit/", views.StudentUpdateView.as_view(), name="student_edit"),
+    path("<int:pk>/delete/", views.StudentDeleteView.as_view(), name="student_delete"),
 ]
--- students/views.py
+++ students/views.py
@@ -1,8 +1,61 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView,
+)
+
+from .models import Student
+from .forms import StudentForm
 
 
-@login_required
-def student_list(request):
-    return render(request, "students/student_list.html")
+class StudentListView(LoginRequiredMixin, ListView):
+    model = Student
+    template_name = "students/student_list.html"
+    context_object_name = "students"
+
+
+class StudentCreateView(LoginRequiredMixin,
+                        SuccessMessageMixin,
+                        CreateView):
+
+    model = Student
+    form_class = StudentForm
+    template_name = "students/student_form.html"
+
+    success_url = reverse_lazy("student_list")
+
+    success_message = "Student added successfully."
+
+
+class StudentUpdateView(LoginRequiredMixin,
+                        SuccessMessageMixin,
+                        UpdateView):
+
+    model = Student
+    form_class = StudentForm
+    template_name = "students/student_form.html"
+
+    success_url = reverse_lazy("student_list")
+
+    success_message = "Student updated successfully."
+
+
+class StudentDeleteView(LoginRequiredMixin,
+                        DeleteView):
+
+    model = Student
+    template_name = "students/student_confirm_delete.html"
+
+    success_url = reverse_lazy("student_list")
+
+
+class StudentDetailView(LoginRequiredMixin,
+                        DetailView):
+
+    model = Student
+    template_name = "students/student_detail.html"
